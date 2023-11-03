9189e1e2a6b1fcfbfb24c25deac0b514fd1236ef
shin <minchae.shin1@gmail.com>
2023-11-03 20:48:51 +0900

    MainController 추가

---
+++ src/main/java/com/spring/practice_board/Controller/MainController.java
@@ -0,0 +1,40 @@
+package com.spring.practice_board.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String root() throws Exception{
+        return "redirect:/home";
+    }
+
+    @GetMapping("/home")
+    public String Home(){
+        return "/WEB-INF/views/home.jsp";
+    }
+
+    @GetMapping("/board")
+    public String Board(){
+        return "/WEB-INF/views/board.jsp";
+    }
+
+    @GetMapping("/member_list")
+    public String Member(){
+        return "/WEB-INF/views/member_list.jsp";
+    }
+
+    @GetMapping("/about")
+    public String About(){
+        return "/WEB-INF/views/about.jsp";
+    }
+
+    @GetMapping("/login")
+    public String Login(){
+        return "/WEB-INF/views/login.jsp";
+    }
+
+}
