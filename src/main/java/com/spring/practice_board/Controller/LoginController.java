fffb99172cabd90abab3d0750547e0f61a914622
shin <minchae.shin1@gmail.com>
2023-11-03 21:59:02 +0900

    LoginController 추가

---
+++ src/main/java/com/spring/practice_board/Controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.spring.practice_board.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class LoginController {
+
+    @GetMapping("/login")
+    public String Login() {
+        return "/WEB-INF/views/login.jsp";
+    }
+
+    @GetMapping("/member_join")
+    public String Memeber_join() {
+        return "/WEB-INF/views/member_join.jsp";
+    }
+}
