0b6bb5676566161989734bc1cdb6b4f6c6da200f
shin <minchae.shin1@gmail.com>
2023-12-09 21:50:00 +0000

    VO객체 생성

---
+++ src/main/java/com/spring/practice_board/VO/VO_board.java
@@ -0,0 +1,48 @@
+package com.spring.practice_board.VO;
+
+public class VO_board {
+
+    private String POSTNUMBER;
+    private String TITLE;
+    private String AUTHOR;
+    private String CREATIONDATE;
+
+    public VO_board(String POSTNUMBER, String TITLE, String AUTHOR, String CREATIONDATE) {
+        this.POSTNUMBER = POSTNUMBER;
+        this.TITLE = TITLE;
+        this.AUTHOR = AUTHOR;
+        this.CREATIONDATE = CREATIONDATE;
+    }
+
+    public String getPOSTNUMBER() {
+        return POSTNUMBER;
+    }
+
+    public void setPOSTNUMBER(String POSTNUMBER) {
+        this.POSTNUMBER = POSTNUMBER;
+    }
+
+    public String getTITLE() {
+        return TITLE;
+    }
+
+    public void setTITLE(String TITLE) {
+        this.TITLE = TITLE;
+    }
+
+    public String getAUTHOR() {
+        return AUTHOR;
+    }
+
+    public void setAUTHOR(String AUTHOR) {
+        this.AUTHOR = AUTHOR;
+    }
+
+    public String getCREATIONDATE() {
+        return CREATIONDATE;
+    }
+
+    public void setCREATIONDATE(String CREATIONDATE) {
+        this.CREATIONDATE = CREATIONDATE;
+    }
+}
