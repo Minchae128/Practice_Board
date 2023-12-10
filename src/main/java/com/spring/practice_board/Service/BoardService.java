7b755c265e490c321c76f2fa1f92a6b016440522
shin <minchae.shin1@gmail.com>
2023-12-10 13:52:00 +0000

    데이터 전달을 위한 VO객체 사용(Service 수정)

--- src/main/java/com/spring/practice_board/Service/BoardService.java
+++ src/main/java/com/spring/practice_board/Service/BoardService.java
@@ -1,6 +1,7 @@
 package com.spring.practice_board.Service;
 
 import com.spring.practice_board.DAO.BoardDao;
+import com.spring.practice_board.VO.VO_board;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,11 +15,16 @@ public class BoardService {
     @Autowired
     BoardDao boardDao;
 
-    public List<Map<String, String>> boardList(){
+/*    public List<Map<String, String>> boardList(){
         List<Map<String, String>> list = new ArrayList<>();
         list = boardDao.boardList();
 
-        return list;
+        return list;*/
 
+    public List<VO_board> boardList(){
+        List<VO_board> list = new ArrayList<>();
+        list = boardDao.boardList();
+
+        return list;
     }
 }
