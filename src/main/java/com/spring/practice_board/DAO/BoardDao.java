1223297248059a89b036ccf090b4a004bbe55f49
shin <minchae.shin1@gmail.com>
2023-12-10 13:51:00 +0000

    데이터 전달을 위한 VO객체 사용(DAO 수정)

--- src/main/java/com/spring/practice_board/DAO/BoardDao.java
+++ src/main/java/com/spring/practice_board/DAO/BoardDao.java
@@ -1,5 +1,6 @@
 package com.spring.practice_board.DAO;
 
+import com.spring.practice_board.VO.VO_board;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -8,5 +9,7 @@ import java.util.Map;
 @Mapper
 public interface BoardDao {
 
-    public List<Map<String, String>> boardList();
+   /* public List<Map<String, String>> boardList();*/
+
+    public List<VO_board> boardList();
 }
